6d54a4050e522acd89b279e7fe34f0dc72512142
JFGaona <gaonajonathan5@gmail.com>
2024-03-06 19:05:05 -0500

    Clases finales agregadas

---
+++ src/EmailSender.java
@@ -0,0 +1,5 @@
+public class EmailSender {
+    public static void sendEmail(Contacto c, String message) {
+//Envía un mensaje la direccion de correo del Contacto c.
+    }
+}
---
+++ src/SMSSender.java
@@ -0,0 +1,5 @@
+public class SMSSender {
+    public static void sendSMS(Contacto c, String message){
+//Envía un mensaje SMS al teléfono del Contacto c.
+    }
+}
